01dbd9757a3b647a6d5cd93f60f9143f53ffde77
18신대성 <esc990720@korea.ac.kr>
2018-12-05 13:11:34 +0900

    add addedit screen and funcs

---
+++ AlcoholList/app/src/main/java/ku/merseong/alcohollist/AddEditActivity.java
@@ -0,0 +1,29 @@
+package ku.merseong.alcohollist;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class AddEditActivity extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_edit_page);
+    }
+
+    public void onBackButtonClicked(View v)
+    {
+        Toast.makeText(this, "onBackButtonClicked", Toast.LENGTH_SHORT).show();
+        finish();
+    }
+
+    public void onSaveButtonClicked(View view)
+    {
+        Toast.makeText(this, "onSaveButtonClicked", Toast.LENGTH_SHORT).show();
+        new Alcohol(1181225, "민들레대포", Enums.AlcCategory.CHEONGJU, "파전", "민들레캐논 마싯서");
+
+        MainActivity.m_oListView.setAdapter(new ListAdapter(Alcohol.publicAlcList()));
+    }
+}
--- AlcoholList/app/src/main/java/ku/merseong/alcohollist/MainActivity.java
+++ AlcoholList/app/src/main/java/ku/merseong/alcohollist/MainActivity.java
@@ -1,12 +1,15 @@
 package ku.merseong.alcohollist;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
 {
-    private ListView m_oListView = null;
+    public static ListView m_oListView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -14,16 +17,16 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // TEST CASES
-        new Alcohol(1111111, "복순도가", Enums.AlcCategory.TAKJU, "NONE", "존나개맛있다.");
-        new Alcohol(1222222, "느린마을", Enums.AlcCategory.TAKJU, "NONE", "존나맛있다.");
-        new Alcohol(1111111, "참이슬", Enums.AlcCategory.SOJU, "NONE", "이걸 왜 돈주고먹음;");
-        new Alcohol(1222222, "청하", Enums.AlcCategory.CHEONGJU, "Defense", "존나맛있다. 방어성공 ㅎㅎ 30자이상테스의ㅏㄴ러아러니");
-        new Alcohol(1333333, "필스너 우르켈", Enums.AlcCategory.BEER, "치킨", "역시 진한맛빳다죠 ㅎㅎ");
-
         m_oListView = (ListView)findViewById(R.id.listview1);
-        ListAdapter oAdapter = new ListAdapter(Alcohol.publicAlcList());
-        m_oListView.setAdapter(oAdapter);
+        m_oListView.setAdapter(new ListAdapter(Alcohol.publicAlcList()));
+    }
+
+    public void onAddButtonClicked(View view)
+    {
+        Toast.makeText(getBaseContext(), "To Add Menu", Toast.LENGTH_LONG).show();
+        // test case
+        Intent intent = new Intent(this, AddEditActivity.class);
+        startActivity(intent);
     }
 }
 
