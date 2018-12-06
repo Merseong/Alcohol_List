053a59161a095fc8e8680dbd786f5a161917ba2f
18신대성 <esc990720@korea.ac.kr>
2018-12-06 11:51:16 +0900

    done edit page xml

--- AlcoholList/app/src/main/java/ku/merseong/alcohollist/AddEditActivity.java
+++ AlcoholList/app/src/main/java/ku/merseong/alcohollist/AddEditActivity.java
@@ -8,7 +8,8 @@ import android.widget.Toast;
 public class AddEditActivity extends Activity
 {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_edit_page);
     }
