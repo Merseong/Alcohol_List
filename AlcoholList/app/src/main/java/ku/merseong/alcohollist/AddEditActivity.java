33406c7f32b66c414374b1611f15f9c973497f4a
18신대성 <esc990720@korea.ac.kr>
2018-12-06 12:34:30 +0900

    add function complete

--- AlcoholList/app/src/main/java/ku/merseong/alcohollist/AddEditActivity.java
+++ AlcoholList/app/src/main/java/ku/merseong/alcohollist/AddEditActivity.java
@@ -3,15 +3,48 @@ package ku.merseong.alcohollist;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class AddEditActivity extends Activity
 {
+    EditText alcName;
+    EditText alcFood;
+    Spinner alcCategory;
+    EditText alcDate;
+    EditText alcComment;
+
+    String str_category = "기타";
+    Enums.AlcCategory category = Enums.AlcCategory.OTHERS;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_edit_page);
+
+        alcName = (EditText)findViewById(R.id.AlcName);
+        alcFood = (EditText)findViewById(R.id.AlcFood);
+        alcCategory = (Spinner)findViewById(R.id.AlcCategory);
+        alcDate = (EditText)findViewById(R.id.AlcDate);
+        alcComment = (EditText)findViewById(R.id.AlcComment);
+
+        alcCategory.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                str_category = parent.getItemAtPosition(position).toString();
+                for (Enums.AlcCategory alc : Enums.AlcCategory.values())
+                    if (alc.getName().equals(str_category)) { category = alc; break;
+                    }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 
     public void onBackButtonClicked(View v)
@@ -23,8 +56,14 @@ public class AddEditActivity extends Activity
     public void onSaveButtonClicked(View view)
     {
         Toast.makeText(this, "onSaveButtonClicked", Toast.LENGTH_SHORT).show();
-        new Alcohol(1181225, "민들레대포", Enums.AlcCategory.CHEONGJU, "파전", "민들레캐논 마싯서");
+        String name = alcName.getText().toString();
+        String food = alcFood.getText().toString();
+        int date = Integer.parseInt(alcDate.getText().toString()) + 1000000;
+        String comment = alcComment.getText().toString();
+
+        new Alcohol(date, name, category, food, comment);
 
         MainActivity.m_oListView.setAdapter(new ListAdapter(Alcohol.publicAlcList()));
+        finish();
     }
 }
